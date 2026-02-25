213ce65dd775c2b186b1f0d927ae1bb3a283e3ec
Mario Reder <mario.reder@pm.me>
2026-02-25 13:06:42 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -19,7 +19,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets",
-  "VITE_CUSTOM_MENUS": "SygnAi Dashboard, https://sygnai.xyz/Dashboard",
+  "VITE_CUSTOM_MENUS": "SygnAi Main, https://sygnai.xyz/Dashboard",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "",
